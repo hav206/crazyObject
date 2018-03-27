e8fe58e6609a813ae6fb6e013728b5dae4a7b4cc
Van Ha <hav206@yahoo.com>
2018-03-26 22:35:09 -0700

    3rd commit- Peformed operations on the crazy object

--- hw_1.js
+++ hw_1.js
@@ -56,7 +56,63 @@ console.log(Object.keys(obj)[1]);
 console.log(Object.keys(obj));
 console.log(Object.values(obj)[1]);
 
-//console.log("hi");
+///////////////////////////////////////////////////////////////
+
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+
+console.log(Object.keys(inception));
+console.log(Object.values(inception));
+
+//////////////////////////////////////////////////////////////
+
+
+const crazyObject = {
+  taco: [{meat: 'steak',
+         cheese: ['panela', 'queso', 'chihuahua']},
+         {meat: 'chicken',
+          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+        ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [{
+      name: "Jeff",
+      occupation: "manager"
+    },
+    {
+      name: "funkhauser",
+      occupation: "tv dude"
+    },
+    {
+      name: "susie",
+      occupation: "jeffs wife",
+      favourtieHobby: "Swearing at Larry and Jeff"
+    },
+    ]
+  }
+}
+
+console.log(crazyObject.taco[1].salsa[5]);
+console.log(crazyObject.larry.quotes[0]);
+console.log(crazyObject.larry.characters[2].favourtieHobby);
+console.log(crazyObject.larry.nicknames[1]);
+console.log(crazyObject.larry.characters[1].name);
 
 
 
