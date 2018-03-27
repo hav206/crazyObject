2134625be5b17fbb869daccddddacd196e7e6ea8
Van Ha <hav206@yahoo.com>
2018-03-26 20:23:30 -0700

    2nd commit - created some data structures and accessed some data

--- hw_1.js
+++ hw_1.js
@@ -41,6 +41,23 @@ listOfClassName1.push(jackName);
 
 console.log(listOfClassName1);
 
+var rainbow = ["blue", "orange", "green", "red", "white"];
+
+for (var i = 0; i < rainbow.length; i++){
+	if (rainbow[i] === "blue")
+		console.log(rainbow[i]);
+		///return rainbow[i];
+	//return -1;
+}
+
+//console.log("test");
+var obj = {food : "Pho", hobby : "FootBall", town: "lynnwood", data : "string"};
+console.log(Object.keys(obj)[1]);
+console.log(Object.keys(obj));
+console.log(Object.values(obj)[1]);
+
+//console.log("hi");
+
 
 
    
